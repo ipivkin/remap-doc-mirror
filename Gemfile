32af30f8db40fc425dcbaeadeaff0c4c1b3e7304
Ivan Pivkin <ipivkin@moysklad.ru>
2019-03-04 18:30:10 +0300

    Updated

--- Gemfile
+++ Gemfile
@@ -1,17 +1,6 @@
-# If you do not have OpenSSL installed, change
-# the following line to use 'http://'
 source 'https://rubygems.org'
 
-# Middleman gem
-gem 'middleman', '>= 4.0.0'
-
-# Develoment mode gems
-group :development do
-  # gem 'middleman-livereload'
-  gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw] # Windows ONLY -> For faster file watcher updates
-  # gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby] # Windows does not come with time zone data
-end
-
-# On development mode run 'bundle install'
-# On production mode run 'bundle install --without development'
-# Bundler ref (groups development / production): http://bundler.io/groups.html
+gem 'middleman', '~> 4.2'
+gem 'middleman-autoprefixer', '~> 2.7.1'
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
--- config.rb
+++ config.rb
@@ -1,74 +1,46 @@
-####
-# GitLab Pages Basic Configuration
-####
+# Activate and configure extensions
+# https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
-###
-# Page options, layouts, aliases and proxies
-###
+activate :autoprefixer do |prefix|
+  prefix.browsers = "last 2 versions"
+end
 
-# With no layout
+# Layouts
+# https://middlemanapp.com/basics/layouts/
+
+# Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-###
-# General configuration
-###
-
-activate :directory_indexes # Pretty urls - instead of 'example.com/about.html' -> 'example.com/about'
-
-###
-# Local: development mode
-###
-
-# Local development config # Run: 'bundle exec middleman server'
-configure :development do
-  # activate :livereload # Reload the browser automatically whenever files change
-  set :base_url, "" # Setting empty baseurl
-end
-
-###
-# Live: production mode
-###
-
-# Build-specific configuration # Run: 'bundle exec middleman build'
-configure :build do
-  activate :minify_css # Minify CSS on build
-  activate :minify_javascript # Minify Javascript on build
-  set :base_url, "/middleman" # baseurl for GitLab Pages (project name) - leave empty if you're building a user/group website
-  set :build_dir, 'public' # set the build directory to GitLab Pages 'public' folder
-  activate :relative_assets # Use relative URLs
-end
-
-#################################
-#################################
-
-####
-# More Options
-####
-
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
+# page '/path/to/file.html', layout: 'other_layout'
 
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
+# Proxy pages
+# https://middlemanapp.com/advanced/dynamic-pages/
 
-###
+# proxy(
+#   '/this-page-has-no-template.html',
+#   '/template-file.html',
+#   locals: {
+#     which_fake_page: 'Rendering a fake page with a local variable'
+#   },
+# )
+
 # Helpers
-###
-
 # Methods defined in the helpers block are available in templates
+# https://middlemanapp.com/basics/helper-methods/
+
 # helpers do
 #   def some_helper
-#     "Helping"
+#     'Helping'
 #   end
 # end
 
-# Redirects
-# redirect "something/index.html", to: "/something/somethingelse"
+# Build-specific configuration
+# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# Setting custom directories for css, js and images
-# set :css_dir, 'assets/css'
-# set :js_dir, 'assets/js'
-# set :images_dir, 'assets/img'
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
